778da7aad023120ffd9c1e580fd5b1f4264faa96
zhaoweibing <zhaoweibing@zhuanzhuan.com>
2017-07-20 17:56:03 +0800

    update path

--- cmd_zz_proto.sh
+++ cmd_zz_proto.sh
@@ -1,5 +1,5 @@
 source_path='/Users/ChildhoddAndy/Documents/ChildhoodAndy/zhuanzhuan/gitlab/Protobuf-Files'
-des_path='/Users/ChildhoddAndy/Documents/ChildhoodAndy/zhuanzhuan/gitlab/ZZIM/ZZIM/Classes/PB'
+des_path='/Users/ChildhoddAndy/ZZPods/ZZIM/ZZIM/Classes/PB'
 
 protoc $source_path/zzbase.proto $source_path/zzuser.proto $source_path/zzmsg.proto --proto_path=$source_path/ --objc_out=$source_path/
 
